cba258081d04b380be1556c513208bddebac0e18
Mariano Molina <xsin.crueldadx@gmail.com>
2015-05-21 00:08:18 -0300

    Video en la home funcionando

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,23 @@
 /* jshint devel:true */
-console.log('\'Allo \'Allo!');
+
+// Dinamic Height
+function calcutaleHeight(element) {
+	'use strict';
+	var windowHeight = $(window).height();
+	var finalHeight = windowHeight - $('header').height();
+	$(element).height(finalHeight);
+}
+
+$(window).load(function() {
+	calcutaleHeight('section');
+});
+
+$(document).ready(function() {
+	'use strict';
+	// resize
+	$(window).resize(function() {
+		calcutaleHeight('section');
+	});
+	// videos 
+	$('#content_video').fitVids();
+});
